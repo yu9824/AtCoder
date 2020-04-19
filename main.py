c5add2074604f35b3c65a3a8f19cd8348433df76
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-04-19 23:10:47 +0900

    ABC163-D

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-import collections
+def main(N, K):
+    def sumN(x):
+        return int(x * (x + 1) * 0.5)
+    ans = sum([sumN(N) - sumN(N - k) - sumN(k - 1) + 1 for k in range(K, N + 2)])
+    print(ans%(10**9+7))
 
-def main(N, A):
-    c = collections.Counter(A)
-    [print(c[i+1]) for i in range(max(c.keys()))]
-    [print(0) for i in range(max(c.keys()), N)]
+
 
 if __name__ == '__main__':
-    N = int(input())
-    A = list(map(int, input().split()))
-    # N = 7
-    # A = list(map(int, "1 2 3 4 5 6".split()))
-    main(N, A)
+    N, K = list(map(int, input().split()))
+    main(N, K)
