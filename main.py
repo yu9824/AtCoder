cf33d44a63904445b3fcab54c6f841de270222a7
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-04-26 22:08:23 +0900

    ABC164-C

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-import math
+def main(N, S):
+    print(len(set(S)))
 
-def main(A, B, C, D):
-    TAKAHASHI = math.ceil(C / B)
-    AOKI = math.ceil(A / D)
-    # print(TAKAHASHI, AOKI)
-    if TAKAHASHI <= AOKI:
-        print('Yes')
-    else:
-        print('No')
 
 if __name__ == '__main__':
-    A, B, C, D = list(map(int, input().split()))
-    main(A, B, C, D)
+    S = []
+    N = int(input())
+    for i in range(N):
+        S.append(input())
+    main(N, S)
