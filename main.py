c0c14ec60ec1e85aa9044fd03af8c0ab11e252d5
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-03 21:26:53 +0900

    ABC166-C

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-def main(N, K, d, A):
-    a = list(set(sum(A, [])))
-    print(N - len(a))
+def main(N, M, H, AB):
+    bad = []
+    for A, B in AB:
+        if H[A - 1] >= H[B - 1]: #pythonは一個ずれる
+            bad.append(B)
+        if H[B - 1] >= H[A - 1]:
+            bad.append(A)
+    print(N - len(list(set(bad))))
 
 if __name__ == '__main__':
-    N, K = list(map(int, input().split()))
-    d = []
-    A = []
-    for i in range(K):
-        d.append(int(input()))
-        A.append(list(map(int, input().split())))
-    main(N, K, d, A)
+    N, M = list(map(int, input().split()))
+    H = list(map(int, input().split()))
+    AB = []
+    for i in range(M):
+        AB.append(list(map(int, input().split())))
+    main(N, M, H, AB)
