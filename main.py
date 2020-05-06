8baf98a97a6ca2a36f2911c4f1ce852ca0e6c7f7
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-06 19:08:43 +0900

    ABC4-C

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-def main(N, L, x, o):
-    '''
-    lは一段目を0, 二段目を1
-    nは一列目を0, 二列目を2...としてる．
-    '''
-    # 初期条件 (oは読み取った時からpython基準なので変換不要)
-    n = o
+def main(N):
+    # 対象の配列を作成
+    A = [str(_ + 1) for _ in range(6)]
 
-    for l in range(L - 1, -1, -1):  # あたり (下) から上へ遡る
-        if OK(n - 1) and x[l][n - 1] == '-':  # 左があみだをはみでないで，かつ横道があるなら
-            n -= 2  # 左の列へ移動
-        elif OK(n + 1) and x[l][n + 1] == '-':  # 右があみだをはみでないで，かつ横道があるなら
-            n += 2  # 右の列へ移動
+    # 最初の配列を保存
+    start = A.copy()
 
-    print(n // 2 + 1)   # nは一列目を0, 二列目を2...としてる．→1, 2, 3, ...に変換
+    # 何回で一周するのかを求める．
+    i = 0
+    while A != start or i == 0:
+        m = i % 5
+        A[m], A[m + 1] = A[m + 1], A[m]
+        i += 1
 
-def OK(n):
-    return 0 <= n and n <= 2 * N - 2
+    # i回の操作で一周することがわかったので
+    A = start.copy()    # 初期化
+    for j in range(N % i):
+        m = j % 5
+        A[m], A[m + 1] = A[m + 1], A[m]
+    print(''.join(A))
 
 
 if __name__ == '__main__':
-    N, L = list(map(int, input().split()))
-    x = [list(input()) for _ in range(L)]
-    o = list(input()).index('o')
-    main(N, L, x, o)
+    N = int(input())
+    main(N)
