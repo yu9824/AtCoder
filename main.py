3d8492db70f79f500e9e876a2fd260d0111f6172
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-06 19:27:06 +0900

    ARC1-A

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def main(a, b, c):
-    for i in range(1, 128):
-        if i % 3 == a and i % 5 == b and i % 7 == c:
-            print(i)
+import collections
+
+def main(N, c):
+    counta = [c.count(str(i + 1)) for i in range(4)]
+    print(max(counta), min(counta))
 
 if __name__ == '__main__':
-    a, b, c = list(map(int, input().split()))
-    main(a, b, c)
+    N = int(input())
+    c = list(input())
+    main(N, c)
