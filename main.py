efbf2c546fd2b31e3a31aa218ef2a6716e17206c
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-06 18:47:29 +0900

    ARC6-B

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-def main(b, a):
-    dict_change = {str(x):str(i) for i, x in enumerate(b)}
-    dict_reverse = {str(i):str(x) for i, x in enumerate(b)}
-    A = list(map(str, sorted([int(j.translate(str.maketrans(dict_change))) for i, j in enumerate(a)])))
-    [print(int(j.translate(str.maketrans(dict_reverse)))) for i, j in enumerate(A)]
+def main(N, L, x, o):
+    '''
+    lは一段目を0, 二段目を1
+    nは一列目を0, 二列目を2...としてる．
+    '''
+    # 初期条件 (oは読み取った時からpython基準なので変換不要)
+    n = o
+
+    for l in range(L - 1, -1, -1):  # あたり (下) から上へ遡る
+        if OK(n - 1) and x[l][n - 1] == '-':  # 左があみだをはみでないで，かつ横道があるなら
+            n -= 2  # 左の列へ移動
+        elif OK(n + 1) and x[l][n + 1] == '-':  # 右があみだをはみでないで，かつ横道があるなら
+            n += 2  # 右の列へ移動
+
+    print(n // 2 + 1)   # nは一列目を0, 二列目を2...としてる．→1, 2, 3, ...に変換
+
+def OK(n):
+    return 0 <= n and n <= 2 * N - 2
 
 
 if __name__ == '__main__':
-    b = list(map(int, input().split()))
-    N = int(input())
-    a = [input() for _ in range(N)]
-    main(b, a)
+    N, L = list(map(int, input().split()))
+    x = [list(input()) for _ in range(L)]
+    o = list(input()).index('o')
+    main(N, L, x, o)
