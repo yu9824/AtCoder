dd289f74860c274be67354dc3c758f195da48836
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-06 19:18:14 +0900

    tenkaichi2012B-A

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
-def main(N):
-    # 対象の配列を作成
-    A = [str(_ + 1) for _ in range(6)]
-
-    # 最初の配列を保存
-    start = A.copy()
-
-    # 何回で一周するのかを求める．
-    i = 0
-    while A != start or i == 0:
-        m = i % 5
-        A[m], A[m + 1] = A[m + 1], A[m]
-        i += 1
-
-    # i回の操作で一周することがわかったので
-    A = start.copy()    # 初期化
-    for j in range(N % i):
-        m = j % 5
-        A[m], A[m + 1] = A[m + 1], A[m]
-    print(''.join(A))
-
+def main(a, b, c):
+    for i in range(1, 128):
+        if i % 3 == a and i % 5 == b and i % 7 == c:
+            print(i)
 
 if __name__ == '__main__':
-    N = int(input())
-    main(N)
+    a, b, c = list(map(int, input().split()))
+    main(a, b, c)
