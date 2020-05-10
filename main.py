50df7b0f4493c129f6cf94055824787d2494eacd
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-10 23:25:51 +0900

    ABC167-D

--- main.py
+++ main.py
@@ -1,37 +1,23 @@
 # list(map(int, input().split()))
 # int(input())
-import itertools
 
-def main(N, M, X, CA):
-    cand = []
-    for n in range(N):  # n個選ぶ
-        for ca in itertools.combinations(CA, n+1):
-            s, price = f(ca)
-            if all(i >= X for i in s):
-                cand.append(price)
-    if len(cand) == 0:
-        print(-1)
+def main(N, K, A):
+    i = 0   # 現在地
+    lst_memo = []
+    set_memo = set([])
+    while i not in set_memo:
+        lst_memo.append(i)
+        set_memo.add(i)
+        i = A[i] - 1    # pythonは一つずれる
+    key = lst_memo.index(i)
+    if K < key:
+        print(lst_memo[K] + 1)
     else:
-        print(min(cand))
-
-
-def f(lst):
-    s = [0 for _ in range(M)]
-    price = 0
-    for ca in lst:
-        price += ca[0]
-        for i in range(M):
-            s[i] += ca[i+1]
-    return s, price
-
-
-
+        roop = lst_memo[key:]
+        print(roop[(K - key) % len(roop)] + 1)    # pythonは一つずれる
 
 
 if __name__ == '__main__':
-    N, M, X = list(map(int, input().split()))
-    CA = []
-    for _ in range(N):
-        CA.append(list(map(int, input().split())))
-
-    main(N, M, X, CA)
+    N, K = list(map(int, input().split()))
+    A = list(map(int, input().split()))
+    main(N, K, A)
