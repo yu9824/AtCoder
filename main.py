356f5af94395daeeee9b75376fa7cd0515da134c
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-10 22:01:05 +0900

    ABC167-C

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 # list(map(int, input().split()))
 # int(input())
+import itertools
 
-
-def main(A, B, C, K):
-    if K - A <= 0:
-        print(K)
-    elif K - A > 0 and K - A - B <= 0:
-        print(A)
+def main(N, M, X, CA):
+    cand = []
+    for n in range(N):  # n個選ぶ
+        for ca in itertools.combinations(CA, n+1):
+            s, price = f(ca)
+            if all(i >= X for i in s):
+                cand.append(price)
+    if len(cand) == 0:
+        print(-1)
     else:
-        print(A - (K - A - B))
+        print(min(cand))
+
+
+def f(lst):
+    s = [0 for _ in range(M)]
+    price = 0
+    for ca in lst:
+        price += ca[0]
+        for i in range(M):
+            s[i] += ca[i+1]
+    return s, price
+
+
+
+
 
 if __name__ == '__main__':
-    A, B, C, K = list(map(int, input().split()))
-    main(A, B, C, K)
+    N, M, X = list(map(int, input().split()))
+    CA = []
+    for _ in range(N):
+        CA.append(list(map(int, input().split())))
+
+    main(N, M, X, CA)
