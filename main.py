0ce26ca316ca25d64cba1184556929a180744c94
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-17 21:22:21 +0900

    ABC168-C

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 # list(map(int, input().split()))
 # int(input())
+from math import cos, pi
 
-def main(K, S):
-    if len(S) > K:
-        answer = S[:K] + '...'
-    else:
-        answer = S
-    print(answer)
-
-
+def main(A, B, H, M):
+    theta_b = M / 60 * (2 * pi)
+    theta_a = 1 / 12 * (H + M / 60) * (2 * pi)
+    AB = (A ** 2 + B ** 2 - 2 * A * B * cos(theta_a - theta_b)) ** 0.5
+    print(AB)
 
 if __name__ == '__main__':
-    K = int(input())
-    S = input()
-    main(K, S)
+    A, B, H, M = map(int, input().split())
+    main(A, B, H, M)
