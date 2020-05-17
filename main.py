720dacb59e6bf7017f0a12628db38d6b47dfe8f6
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-17 21:02:48 +0900

    ABC168-A

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 # list(map(int, input().split()))
 # int(input())
 
-def main(N, K, A):
-    i = 0   # 現在地
-    lst_memo = []
-    set_memo = set([])
-    while i not in set_memo:
-        lst_memo.append(i)
-        set_memo.add(i)
-        i = A[i] - 1    # pythonは一つずれる
-    key = lst_memo.index(i)
-    if K < key:
-        print(lst_memo[K] + 1)
+def main(N):
+    a = N % 10
+    if a in [2, 4, 5, 7, 9]:
+        print('hon')
+    elif a == 3:
+        print('bon')
     else:
-        roop = lst_memo[key:]
-        print(roop[(K - key) % len(roop)] + 1)    # pythonは一つずれる
+        print('pon')
 
 
 if __name__ == '__main__':
-    N, K = list(map(int, input().split()))
-    A = list(map(int, input().split()))
-    main(N, K, A)
+    N = int(input())
+    main(N)
