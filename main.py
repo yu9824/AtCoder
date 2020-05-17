5bbca071217bc85c3d53e4f7bbd9306729b3adf1
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-17 21:07:17 +0900

    ABC168-B

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # list(map(int, input().split()))
 # int(input())
 
-def main(N):
-    a = N % 10
-    if a in [2, 4, 5, 7, 9]:
-        print('hon')
-    elif a == 3:
-        print('bon')
+def main(K, S):
+    if len(S) > K:
+        answer = S[:K] + '...'
     else:
-        print('pon')
+        answer = S
+    print(answer)
+
 
 
 if __name__ == '__main__':
-    N = int(input())
-    main(N)
+    K = int(input())
+    S = input()
+    main(K, S)
