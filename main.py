c7c23135ab8391dfec4c63f31cb3ed6806843795
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-23 13:03:48 +0900

    PAST3-A

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # list(map(int, input().split()))
 # int(input())
-from math import cos, pi
+def main(s, t):
+    if s == t:
+        print('same')
+    elif s.upper() == t.upper():
+        print('case-insensitive')
+    else:
+        print('different')
 
-def main(A, B, H, M):
-    theta_b = M / 60 * (2 * pi)
-    theta_a = 1 / 12 * (H + M / 60) * (2 * pi)
-    AB = (A ** 2 + B ** 2 - 2 * A * B * cos(theta_a - theta_b)) ** 0.5
-    print(AB)
 
 if __name__ == '__main__':
-    A, B, H, M = map(int, input().split())
-    main(A, B, H, M)
+    s = input()
+    t = input()
+    main(s, t)
