63e62a942218b3064ad46b3501d8bcaab5b20af5
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-23 16:07:06 +0900

    PAST3-F

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 # list(map(int, input().split()))
 # int(input())
 
-def main(N, M, Q, rinsetu, c):
-    s = []
-    for _ in range(Q):
-        t = list(map(int, input().split()))
-        if t[0] == 1:   # クエリ1(スプリンクラー起動)の場合
-            x = t[1]
-            # x番目のスプリンクラーの現在の色を出力
-            print(c[x - 1])
-            # x番目のスプリンクラーの色で隣接スプリンクラーの色を上塗り
-            for r in rinsetu[x - 1]:
-                c[r] = c[x - 1]
-        else:   # クエリ2(色を上塗り)の場合
-            x, y = t[1:]
-            # x番目のスプリンクラーの現在の色を出力
-            print(c[x - 1])
-            # x番目のスプリンクラーの色を色yで上塗り
-            c[x - 1] = y
+def main(N):
+    if N == 1:
+        print(input())
+    else:
+        # 小文字英字のリストを作成
+        alpha = [chr(i) for i in range(97, 97+26)]
 
+        # 何回出てきたかをカウントしておくリスト
+        counta = [[0 for _ in range(26)] for __ in range(N)]
+
+        for n in range(N):
+            a = list(input())
+            for s in a:
+                counta[n][alpha.index(s)] += 1
+
+        ans = []
+        for n in range(N // 2):
+            for i in range(26):
+                # 上からn番目としたからn + 1番目両方に出てくるアルファベットがあるならば
+                if counta[n][i] > 0 and counta[-(n + 1)][i] > 0:
+                    ans[n:n] = [alpha[i], alpha[i]]
+                    counta[n][i] -= 1
+                    counta[-(n + 1)][i] -= 1
+                    break
+                elif i == 25:   # 一回も条件を満たすことなくここまできたら．
+                    print(-1)
+                    exit()
+        if len(ans) + 1 == N:
+            for i in range(26):
+                if counta[N // 2][i] > 0:
+                    ans.insert(N // 2, alpha[i])
+                    break
+        print(''.join(ans))
 
 
 if __name__ == '__main__':
-    N, M, Q = list(map(int, input().split()))
-    rinsetu = [[] for _ in range(N)]
-    for _ in range(M):
-        u, v = list(map(int, input().split()))
-        rinsetu[u - 1].append(v - 1)
-        rinsetu[v - 1].append(u - 1)
-    c = list(map(int, input().split()))
-    main(N, M, Q, rinsetu, c)
+    N = int(input())
+    main(N)
