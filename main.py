816a4eaf0b56514a090f5919486e2d5056159c8e
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-31 23:34:06 +0900

    ABC169-B

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 # list(map(int, input().split()))
 # int(input())
 
-def main():
-    A, B = list(map(int, input().split()))
-    print(A * B)
+def main(N):
+    A = list(map(int, input().split()))
+    st_A = set(A)
+    if 0 in st_A:
+        print(0)
+    else:
+        x = 1
+        for a in A:
+            x *= a
+            if x > 10 ** 18:
+                print(-1)
+                exit()
+        else:
+            print(x)
+
+
 
 
 
 if __name__ == '__main__':
-    main()
+    N = int(input())
+    main(N)
