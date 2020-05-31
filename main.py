4d11e0ab081196390edfa51bc68e9486d2047e47
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-05-31 21:01:43 +0900

    ABC169-A

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 # list(map(int, input().split()))
 # int(input())
 
-def main(T):
-    ans = list(T)
-    for i, s in enumerate(T):
-        if s == '?':
-            ans[i] = 'D'
-    print(''.join(ans))
-    # for i, s in enumerate(T):
-    #     if s == '?':
-    #         if T[i+1] != 'P':
-    #             ans[i] = 'P'
-    #         else:
-    #             ans[i] = 'D'
-    # print(''.join(ans))
+def main():
+    A, B = list(map(int, input().split()))
+    print(A * B)
+
 
 
 if __name__ == '__main__':
-    T = input()
-    main(T)
+    main()
