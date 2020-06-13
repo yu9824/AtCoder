a474a19098905ffcf2a95befe06674a4e74d68ba
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-06-13 21:07:39 +0900

    tokyokaijo2020-B

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 # list(map(int, input().split()))
 # int(input())
-def main():
-    S =input()
-    print(S[:3])
+def main(A, V, B, W, T):
+    dist = abs(A - B)
+    velo = V - W
+    if dist <= velo * T:
+        print('YES')
+    else:
+        print('NO')
 
 if __name__ == '__main__':
-    main()
+    A, V = list(map(int, input().split()))
+    B, W = list(map(int, input().split()))
+    T = int(input())
+    main(A, V, B, W, T)
