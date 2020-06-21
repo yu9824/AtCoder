d345ae456c71112d00f414c74c3328cc2084d929
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-06-21 21:01:29 +0900

    ABC171-A

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 # list(map(int, input().split()))
 # int(input())
 
-
-def main(X, N, P):
-    st = set(P)
-    if N == 0:
-        print(X)
+def main():
+    if input().isupper():
+        print('A')
     else:
-        n = 0
-        while True:
-            if X - n not in st:
-                print(X - n)
-                exit()
-            elif X + n not in st:
-                print(X + n)
-                exit()
-            else:
-                n += 1
+        print('a')
 
 if __name__ == '__main__':
-    X, N = list(map(int, input().split()))
-    P = list(map(int, input().split()))
-    main(X, N, P)
+    main()
