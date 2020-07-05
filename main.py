9107fae8878fd6959a1fec4d341c0e51c3febe70
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-07-05 22:24:48 +0900

    ABC173-D

--- main.py
+++ main.py
@@ -1,46 +1,40 @@
 # list(map(int, input().split()))
 # int(input())
-from itertools import combinations
 
-def main(H, W, K, HW):
-    ans = 0
+from collections import deque
 
-    # もとのHWに含まれる黒の数を数える．
-    n_all = sum(HW, []).count('#')
+def main(N, A):
+    # 席順
+    l = A[:2]
+    # 心地よさ合計
+    ans = A[0]
 
-    # 組み合わせ生成
-    comb_h = []
-    comb_w = []
-    for h in range(H):
-        comb_h.extend(combinations([i for i in range(H)], h))
-    for w in range(W):
-        comb_w.extend(combinations([i for i in range(W)], w))
+    d = deque([A[1], A[1]])
 
-    # h行目にある黒の数を数える．
-    for c_h in comb_h:
-        n_h = 0
-        for h in c_h:
-            n_h += HW[h].count('#')
-
-        # w行目にある黒の数を数える．
-        for c_w in comb_w:
-            n_w = 0
-            n_hw = 0
-            for w in c_w:
-                for h in range(H):
-                    if HW[h][w] == '#':
-                        n_w += 1
-                        if h in c_h:
-                            n_hw += 1
-            # print(c_h, c_w, n_h, n_w, n_hw)
-            n_black = n_all - n_h - n_w + n_hw
-            if n_black == K:
-                ans += 1
+    for a in A[2:]:
+        ans += d.popleft()
+        d.extend([a, a])
     print(ans)
 
+
+
 if __name__ == '__main__':
-    H, W, K = list(map(int, input().split()))
-    HW = []
-    for h in range(H):
-        HW.append(list(input()))
-    main(H, W, K, HW)
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    # import numpy as np
+    # N = 2
+    # A = list(np.arange(N, dtype = int) + 1)
+
+
+    # import time
+    # import numpy as np
+    # N = 10 ** 5
+    # A = list((np.random.rand(N) * 10 ** 9).astype(int))
+    #
+    # print('生成完了！')
+    # t1 = time.time()
+    A.sort(reverse = True)
+    main(N, A)
+    # t2 = time.time()
+    # print(t2 - t1)
