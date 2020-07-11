f406c8752d7dec223838083a7d33eef079b3883a
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-07-11 21:33:41 +0900

    AISing2020-C

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # list(map(int, input().split()))
 # int(input())
 
-def main(N, A):
-    ans = 0
-    for n in range(1, N+1, 2):
-        if A[n-1] % 2 == 1:
-            ans += 1
-    print(ans)
+from collections import Counter
+
+def main(N, X):
+    lst = list(X)
+    num = lst.count('1')
+    print(num)
+    # for n in range(N):
 
 
 if __name__ == '__main__':
     N = int(input())
-    A = list(map(int, input().split()))
-    main(N, A)
+    X = int(input())
+    main(N, X)
