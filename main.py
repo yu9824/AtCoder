366e431f340e1fe6cfcf010f36fc5121349a6b9d
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-07-11 21:02:02 +0900

    AISing2020-A

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
 # list(map(int, input().split()))
 # int(input())
 
-from collections import deque
+def main():
+    L, R, d = list(map(int, input().split()))
 
-def main(N, A):
-    # 席順
-    l = A[:2]
-    # 心地よさ合計
-    ans = A[0]
-
-    d = deque([A[1], A[1]])
-
-    for a in A[2:]:
-        ans += d.popleft()
-        d.extend([a, a])
+    ans = 0
+    for i in range(L, R+1):
+        if i % d == 0:
+            ans += 1
     print(ans)
 
 
-
 if __name__ == '__main__':
-    N = int(input())
-    A = list(map(int, input().split()))
-
-    # import numpy as np
-    # N = 2
-    # A = list(np.arange(N, dtype = int) + 1)
-
-
-    # import time
-    # import numpy as np
-    # N = 10 ** 5
-    # A = list((np.random.rand(N) * 10 ** 9).astype(int))
-    #
-    # print('生成完了！')
-    # t1 = time.time()
-    A.sort(reverse = True)
-    main(N, A)
-    # t2 = time.time()
-    # print(t2 - t1)
+    main()
