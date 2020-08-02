98eae0186630be8d1b3cfe129580cf15b93c1d3d
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-08-02 21:11:02 +0900

    ABC174-B

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 # int(input())
 
 def main():
-    X = int(input())
-    if X >= 30:
-        print('Yes')
-    else:
-        print('No')
-
+    N, D = list(map(int, input().split()))
+    counta = 0
+    D_2 = D ** 2
+    for _ in range(N):
+        x, y = list(map(int, input().split()))
+        if x ** 2 + y ** 2 <= D_2:
+            counta += 1
+    print(counta)
 
 if __name__ == '__main__':
     main()
