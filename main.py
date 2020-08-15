37b30f958e660bbe4478f2bff190bb82d5367c78
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-08-15 21:15:58 +0900

    ABC175-B

--- main.py
+++ main.py
@@ -2,15 +2,16 @@
 # int(input())
 
 def main():
-    S = input()
-    if 'RRR' in S:
-        print(3)
-    elif 'RR' in S:
-        print(2)
-    elif 'R' in S:
-        print(1)
-    else:
-        print(0)
+    N = int(input())
+    L = sorted(list(map(int, input().split())))
+
+    cnt = 0
+    for i, x in enumerate(L[:-2]):
+        for j, y in enumerate(L[i+1:-1]):
+            for z in L[i+j+2:]:
+                if len({x, y, z}) == 3:
+                    cnt += int(x + y > z)
+    print(cnt)
 
 if __name__ == '__main__':
     main()
