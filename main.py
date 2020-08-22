0466588a9a2998b4fddeb4b6e928b7ed665e06e4
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-08-22 21:02:31 +0900

    ABC176-A

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
 # list(map(int, input().split()))
 # int(input())
+from math import ceil
 
-def main(X, K, D):
-    x = abs(X)
-    k = K
-
-    shou, amari = x // D, x % D
-    if shou >= k:   # 0付近までたどり着かないとき
-        x -= k * D
-        k = 0
-    else:
-        k -= (shou + 1)
-        x -= (shou + 1) * D
-
-    if k % 2:    # 奇数のとき
-        print(abs(x + D))
-    else:   # 偶数の時
-        print(abs(x))
+def main():
+    N, X, T = list(map(int, input().split()))
+    print(ceil(N / X) * T)
 
 if __name__ == '__main__':
-    X, K, D = list(map(int, input().split()))
-    main(X, K, D)
+    main()
