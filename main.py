10004191893384a1b57198c045ae6730ad67749d
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-08-23 11:10:51 +0900

    ABC176-D

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 # list(map(int, input().split()))
 # int(input())
 
-# ****** PyPy3だったら通った． Pythonは落ちた ******
-from collections import Counter
+'''
+PythonだとTLE
+PyPyでも一つだけ (random_05.txtだけTLE)だけどこれ以上高速化する方法がわからない．
+'''
 
-def main(H, W, M, obj_h, obj_w, obj):
-    c_h = Counter(obj_h)
-    c_w = Counter(obj_w)
-    max_h = max(c_h.values())
-    max_w = max(c_w.values())
-    lst_h = [kv[0] for kv in c_h.items() if kv[1] == max_h]
-    lst_w = [kv[0] for kv in c_w.items() if kv[1] == max_w]
-    for h in lst_h:
-        for w in lst_w:
-            if (h, w) not in obj:
-                print(max_h + max_w)
-                exit()
-    else:
-        print(max_h + max_w - 1)
+def main(H, W, C, D, S):
+    def inside(x, y):
+        return ((0 <= x and x < H) and (0 <= y and y < W))
 
+    next = {C}
+    masu = [[-1 for _ in range(W)] for __ in range(H)]
+
+    def moverange(x, y):
+        return inside(x, y) and masu[x][y] == -1 and S[x][y] == '.'
+
+    def func(now, kaisu):
+        x, y = now
+        for canmove in {(x-1, y), (x+1, y), (x, y+1), (x, y-1)}:
+            if moverange(*canmove):
+                que.add(canmove)
+                masu[canmove[0]][canmove[1]] = kaisu
+
+    def make_st(x, y):
+        return {(x+i, y+j) for i in range(-2, 3) for j in range(-2, 3)}
+
+    kaisu = -1
+    while len(next) != 0:
+        kaisu += 1
+        que = next
+        next = set()
+        while que:
+            now = que.pop()
+            if moverange(*now):
+                masu[now[0]][now[1]] = kaisu
+            func(now, kaisu)
+
+            for canmove in make_st(*now):
+                if moverange(*canmove):
+                    next.add(canmove)
+    print(masu[D[0]][D[1]])
 
 
 if __name__ == '__main__':
-    H, W, M = list(map(int, input().split()))
-    obj = set()
-    obj_h = []
-    obj_w = []
-    for _ in range(M):
-        i = list(map(int, input().split()))
-        obj.add(tuple(i))
-        obj_h.append(i[0])
-        obj_w.append(i[1])
-    main(H, W, M, obj_h, obj_w, obj)
-
-# def main(H, W, Ch, Cw, Dh, Dw, S):
-#
-#
-# if __name__ == '__main__':
-#     # N = int(input())
-#     # A = list(map(int, input().split()))
-#     H, W = list(map(int, input().split()))
-#     Ch, Cw = list(map(int, input().split()))
-#     Dh, Dw = list(map(int, input().split()))
-#     S = []
-#     for _ in range(H):
-#         S.append(input())
-#     main(H, W, Ch, Cw, Dh, Dw, S)
+    H, W = list(map(int, input().split()))
+    C = tuple(map(lambda x:int(x)-1, input().split()))
+    D = tuple(map(lambda x:int(x)-1, input().split()))
+    S = [input() for _ in range(H)]
+    main(H, W, C, D, S)
