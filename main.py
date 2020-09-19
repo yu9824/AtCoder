4fb6a654a5c3d99463e6e9ec64e5a79bd648f9c4
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-09-19 21:08:29 +0900

    ABC179-B

--- main.py
+++ main.py
@@ -2,11 +2,19 @@
 # int(input())
 
 def main():
-    S = input()
-    if S[-1] == 's':
-        print(S + 'es')
+    N = int(input())
+    cnt = 0 # 連続ゾロ目カウンター
+    ans = 'No'
+
+    for n in range(N):
+        if len(set(map(int, input().split()))) == 1: # ゾロ目だったら
+            cnt += 1
+        else:
+            cnt = 0
+        if cnt >= 3:    # 三回連続で出たら
+            ans = 'Yes'
     else:
-        print(S + 's')
+        print(ans)
 
 if __name__ == '__main__':
     main()
