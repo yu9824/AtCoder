8dc15c34501da9e5dfb912fef92db6b7dfde4aca
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-09-19 21:02:13 +0900

    ABC179-A

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # list(map(int, input().split()))
 # int(input())
 
-# 余事象と見れなかった時点で絶望的
-
-mod = 10 ** 9 + 7
-def main(N):
-    print((10 ** N - (9 ** N) * 2 + (8 ** N)) % mod)
-
+def main():
+    S = input()
+    if S[-1] == 's':
+        print(S + 'es')
+    else:
+        print(S + 's')
 
 if __name__ == '__main__':
-    N = int(input())
-    main(N)
+    main()
