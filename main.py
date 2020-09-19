54b672836eafdca98a5fb5196034b31dcde1a1ed
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-09-19 22:46:53 +0900

    ABC179-E

--- main.py
+++ main.py
@@ -2,54 +2,39 @@
 # int(input())
 
 
-# Pythonだと通らなかったけどPyPy3だと通った．
+def main(N, X, M):
+    kurikaesi = []
+    shoki = [X]
+    st_shoki = {X}
 
-def get_sieve_of_eratosthenes(n):
-    if not isinstance(n, int):
-        raise TypeError("n is int-type.")
-    if n < 2:
-        raise ValueError("n is more than 2")
-    data = [i for i in range(2, n + 1)]
-    for d in data:
-        data = [x for x in data if (x == d or x % d != 0)]
-    return data
-
-p_list = get_sieve_of_eratosthenes(10**3)
-
-def factorization_counta(n):
-    arr = []
-    temp = n
-    for i in p_list:
-        if i * i > n:
+    now = X
+    for n in range(1, N):
+        next = (now ** 2) % M
+        if next in st_shoki:
+            i = shoki.index(next)
+            kurikaesi = shoki[i:]
+            shoki = shoki[:i]
             break
-        elif temp % i == 0:
-            cnt = 0
-            while temp % i == 0:
-                cnt += 1
-                temp //= i
-            arr.append(cnt)
+        else:
+            shoki.append(next)
+            st_shoki.add(next)
+            now = next
+    else:
+        print(sum(shoki))
+        exit()
 
-    if temp != 1:
-        arr.append(1)
-    if arr == []:
-        arr.append(1)
-    return arr
+    shou = (N - len(shoki)) // len(kurikaesi)
+    amari = (N - len(shoki)) % len(kurikaesi)
 
-def get_number_comb(arr):
-    ans = 1
-    for i in arr:
-        ans *= (i + 1)
-    return ans
-
-# 約数の数 == その数に掛け算でなるための組み合わせの数
-
-def main(N):
-    ans = 1
-    for n in range(2, N):
-        ans += get_number_comb(factorization_counta(n))
-    print(ans)
-
+    print(sum(shoki) + sum(kurikaesi) * shou + sum(kurikaesi[:amari]))
 
 if __name__ == '__main__':
-    N = int(input())
-    main(N)
+    # N, K = list(map(int, input().split()))
+    # S = set()
+    # for k in range(K):
+    #     L, R = list(map(int, input().split()))
+    #     S = S | {i for i in range(L, R+1)}
+
+    N, X, M = list(map(int, input().split()))
+    # N, X, M = 6, 2, 1001
+    main(N, X, M)
