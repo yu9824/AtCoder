2d2d85637b6a506c1a09daa09672fe6568b47f53
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-09-26 22:13:59 +0900

    ABL-B

--- main.py
+++ main.py
@@ -4,17 +4,6 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-def main(A, B, C, D):
-    if D < A or B < C:
-        print('No')
-    else:
-        print('Yes')
-
-
-if __name__ == '__main__':
-    A, B, C, D = list(map(int, input().split()))
-    main(A, B, C, D)
-
 
 # def main(N, K, A):
 #     N, K, A
@@ -25,12 +14,15 @@ if __name__ == '__main__':
 #     main(N, K, A)
 
 
-# mod = 998244353
-#
-# def main(N, Q, LRD):
-#
-#
-# if __name__ == '__main__':
-#     N, Q = list(map(int, input().split()))
-#     LRD = [tuple(map(int, input().split())) for q in range(Q)]
-#     main(N, Q, LRD)
+mod = 998244353
+
+def main(N, Q, LRD):
+    s = '1' * N
+    print(s)
+    # for l, r, d in LRD:
+
+
+if __name__ == '__main__':
+    N, Q = list(map(int, input().split()))
+    LRD = [tuple(map(int, input().split())) for q in range(Q)]
+    main(N, Q, LRD)
