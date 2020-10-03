5ec210a71e9bed1238d81600a981b20312ceb2ca
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-10-03 22:23:52 +0900

    ARC104-B

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
 # list(map(int, input().split()))
 # int(input())
 
+from pdb import set_trace
+
+from copy import copy
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-
-# def main(N, K, A):
-#     N, K, A
-#
-# if __name__ == '__main__':
-#     N, K = list(map(int, input().split()))
-#     A = [int(input()) for n in range(N)]
-#     main(N, K, A)
+# 累積和?
 
 
-mod = 998244353
+def main(N, S):
+    p1 = {'G', 'C'}
+    p2 = {'A', 'T'}
+    p = ['A', 'T', 'G', 'C']
 
-def main(N, Q, LRD):
-    s = '1' * N
-    print(s)
-    # for l, r, d in LRD:
+    memo = [0, 0, 0, 0]
+    ruiseki = [copy(memo)]
+    ans = 0
+    for i in range(N):
+        memo[p.index(S[i])] += 1
+        ruiseki.append(copy(memo))
+
+    def subtract(lst1, lst2, i):
+        return lst1[i] - lst2[i]
+
+    for j in range(N+1-2):
+        for k in range(j+2, N+1):
+            lst1 = ruiseki[k]
+            lst2 = ruiseki[j]
+            if subtract(lst1, lst2, 0) == subtract(lst1, lst2, 1) and subtract(lst1, lst2, 2) == subtract(lst1, lst2, 3):
+                ans += 1
+    print(ans)
+
+
+
+    # set_trace()
+
 
 
 if __name__ == '__main__':
-    N, Q = list(map(int, input().split()))
-    LRD = [tuple(map(int, input().split())) for q in range(Q)]
-    main(N, Q, LRD)
+    # N = 5000
+    # S = 'ATGC' * 1250
+    # print(S)
+
+    N, S = input().split()
+    N = int(N)
+
+    # N, S = 4, 'AGCT'
+
+
+    main(int(N), S)
