15dcf05a877bd37d82595dfd64ed3652e3f8962d
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-10-10 21:42:56 +0900

    HHKB2020-C

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 # list(map(int, input().split()))
 # int(input())
 
-from pdb import set_trace
-
-def main():
-    S = input()
-    T = input()
-    if S == 'Y':
-        print(T.upper())
-    else:
-        print(T)
+def main(N, P):
+    minimum = 0
+    st = set()
+    for p in P:
+        if p != minimum:
+            print(minimum)
+            st.add(p)
+        else:   # つまり p == minimum
+            minimum += 1
+            while minimum in st:
+                st.remove(minimum)
+                minimum += 1
+            else:
+                print(minimum)
 
 if __name__ == '__main__':
-    main()
+    N = int(input())
+    P = list(map(int, input().split()))
+    main(N, P)
