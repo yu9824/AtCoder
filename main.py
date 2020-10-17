3e81794dfe6d85fb10b29abb9c465b41b8a741c2
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-10-17 20:02:19 +0900

    ABC180-A

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
 # list(map(int, input().split()))
 # int(input())
 
-def main(N, P):
-    minimum = 0
-    st = set()
-    for p in P:
-        if p != minimum:
-            print(minimum)
-            st.add(p)
-        else:   # つまり p == minimum
-            minimum += 1
-            while minimum in st:
-                st.remove(minimum)
-                minimum += 1
-            else:
-                print(minimum)
+def main():
+    N, A, B = list(map(int, input().split()))
+    print(N-A+B)
 
 if __name__ == '__main__':
-    N = int(input())
-    P = list(map(int, input().split()))
-    main(N, P)
+    main()
