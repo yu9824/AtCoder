4aeb3f679bb35508b8fc57f01b7dcd46e5e0b79b
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-10-25 13:58:20 +0900

    Add library.py (UnionFind)

---
+++ library.py
@@ -0,0 +1,18 @@
+
+
+class UnionFind:
+    def __init__(self, N):
+        self.root = [n for n in range(N)]
+
+    def get_root(self, x):
+        if self.root[x] == x:
+            return x
+        else:
+            self.root[x] = self.get_root(self.root[x])
+            return self.root[x]
+
+    def unite(self, x, y):
+        root_x = self.get_root(x)
+        root_y = self.get_root(y)
+        if root_x != root_y:
+            self.root[root_x] = root_y
