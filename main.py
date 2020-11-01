3e81c966ecf9086ecc953611003b5b18f0d835be
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-01 16:05:32 +0900

    ARC107-C

--- main.py
+++ main.py
@@ -4,45 +4,59 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-'''
-(a + b) - (c + d)と考える．それぞれab, cd
-これの取れる範囲は 2から2 * N
-'''
+import numpy as np
+from math import factorial
+from collections import Counter
+
+class UnionFind:
+    def __init__(self, N):
+        self.root = [n for n in range(N)]
+
+    def get_root(self, x):
+        if self.root[x] == x:
+            return x
+        else:
+            self.root[x] = self.get_root(self.root[x])
+            return self.root[x]
+
+    def unite(self, x, y):
+        root_x = self.get_root(x)
+        root_y = self.get_root(y)
+        if root_x != root_y:
+            self.root[root_x] = root_y
+
+mod = 998244353
 
 def main(*args):
-    N, K = args
+    N, K, A = args
 
-    # 範囲
-    l = 2
-    r = 2 * N
+    tate = UnionFind(N)
+    yoko = UnionFind(N)
+    def solve(A, uf):
+        for i in range(N):
+            for j in range(0, i):
+                if np.all((A[i] + A[j]) <= K):
+                    uf.unite(i, j)
+    solve(A, yoko)
+    solve(A.T, tate)
 
-    ans = 0
-
-    cd = l
-    ab = cd + K
-    while ab <= r:
-        # print(ab, cd)
-        if ab - 1 > N:
-            num_ab = max(N - (ab - N) + 1, 0)
-        else:
-            num_ab = max(ab - 1, 0)
-
-        if cd - 1 > N:
-            num_cd = max(N - (cd - N) + 1, 0)
-        else:
-            num_cd = max(cd - 1, 0)
-            
-        ans += num_ab * num_cd
-
-        # 次の繰り返しに向けて．
-        ab += 1
-        cd += 1
+    get_group = lambda uf: [element for element in Counter(uf.get_root(n) for n in range(N)).values() if element != 1]
+    group_yoko = get_group(yoko)
+    group_tate = get_group(tate)
+    
+    components =list(map(factorial, group_yoko + group_tate))
+    
+    ans = 1
+    while components:
+        ans *= components.pop(0) % mod
+        ans %= mod
     print(ans)
-    
-    
 
 if __name__ == '__main__':
     args = list(map(int, input().split()))
+    N = args[0]
+    A = np.array([list(map(int, input().split())) for n in range(N)])
+    args.append(A)
     main(*args)
 
 
