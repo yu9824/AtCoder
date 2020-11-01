67c9f048bb3ea4f4b9e131cd5793c85472a48e9d
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-01 21:58:17 +0900

    ABC181-D

--- main.py
+++ main.py
@@ -1,31 +1,41 @@
 # list(map(int, input().split()))
 # int(input())
 
-import sys
-sys.setrecursionlimit(10 ** 9)
+# import sys
+# sys.setrecursionlimit(10 ** 9)
 
-from itertools import combinations
+from itertools import permutations
 
 def main(*args):
-    N, XY = args
+    S = args[0]
+    counts = [0 for n in range(10)]
+    boolean = [False if n != 0 else True for n in range(10)]
+    
+    for s in S:
+        int_s = int(s)
+        if counts[int_s] < 3:
+            counts[int_s] += 1
+            if counts[int_s] == 3:
+                boolean[int_s] = True
+                if sum(boolean) == 10:
+                    break
 
-    def difference(p1, p2):
-        return p2[0] - p1[0], p2[1] - p1[1]
+    l = []
+    for i, c in enumerate(counts):
+        l += [str(i)] * c
+    
+    S = l
 
-    for c in combinations(XY, r = 3):
-        p1, p2, p3 = c  # p = (x, y)
-        diff12 = difference(p1, p2)
-        diff13 = difference(p1, p3)
-        if diff12[1] * diff13[0] == diff13[1] * diff12[0]:
+    p = permutations(S, min(len(S), 3))
+    for c in set(p):
+        if int(''.join(c)) % 8 == 0:
             print('Yes')
             break
     else:
-        print('No')
+        print('No') 
     
 
 if __name__ == '__main__':
-    N = int(input())
-    args = [N]
-    args.append({tuple(map(int, input().split())) for n in range(N)})
+    S = input()
+    args = [S]
     main(*args)
-    
