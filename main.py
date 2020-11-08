fc11623ddef1c2c3e03637b3328132f53cffa0e2
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-08 22:51:04 +0900

    ABC182-D

--- main.py
+++ main.py
@@ -4,45 +4,23 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-from collections import Counter
 
 def main(*args):
-    lst = args[0]
-    l = len(lst)
-
-    s = sum(lst) % 3
-    if s == 0:
-        print(0)
-    else:
-        c = Counter(lst)
-        if s == 1:
-            if 1 in c:
-                if l == 1:
-                    print(-1)
-                else:
-                    print(1)
-            elif 2 in c and c[2] >= 2:
-                if l <= 2:
-                    print(-1)
-                else:
-                    print(2)
-            else:
-                print(-1)
-        else:   # s == 2
-            if 2 in c:
-                if l == 1:
-                    print(-1)
-                else:
-                    print(1)
-            elif 1 in c and c[1] >= 2:
-                if l <= 2:
-                    print(-1)
-                else:
-                    print(2)
-            else:
-                print(-1)
-            
+    N, A = args
+    
+    r = 0   # 通る最大座標．
+    x = 0   # 一通りの操作がおわったときの　座標
+    p = 0   # 合計の座標の変化
+    q = 0   # 初期値が0として一通りの操作をやったときに通る最大座標
+    for a in A:
+        p += a
+        q = max(q, p)
+        r = max(r, x + q)
+        x += p
+    print(max(r, 0))
 
 if __name__ == '__main__':
-    args = [[int(i) % 3 for i in input()]]
+    N = int(input())
+    args = [N]
+    args.append(list(map(int, input().split())))
     main(*args)
