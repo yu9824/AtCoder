a0eaffc8f6fe71aaa876b3c29e96268585d83908
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-08 21:32:32 +0900

    ABC182-C

--- main.py
+++ main.py
@@ -4,24 +4,45 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-def main(*args):
-    N, A = args
+from collections import Counter
 
-    ans = 0
-    maximum = 0
-    for i in range(2, max(A)+1):
-        cnt = 0
-        for a in A:
-            if a % i == 0:
-                cnt += 1
-        else:
-            if cnt > maximum:
-                maximum = cnt
-                ans = i
-    print(ans)
-    
+def main(*args):
+    lst = args[0]
+    l = len(lst)
+
+    s = sum(lst) % 3
+    if s == 0:
+        print(0)
+    else:
+        c = Counter(lst)
+        if s == 1:
+            if 1 in c:
+                if l == 1:
+                    print(-1)
+                else:
+                    print(1)
+            elif 2 in c and c[2] >= 2:
+                if l <= 2:
+                    print(-1)
+                else:
+                    print(2)
+            else:
+                print(-1)
+        else:   # s == 2
+            if 2 in c:
+                if l == 1:
+                    print(-1)
+                else:
+                    print(1)
+            elif 1 in c and c[1] >= 2:
+                if l <= 2:
+                    print(-1)
+                else:
+                    print(2)
+            else:
+                print(-1)
+            
 
 if __name__ == '__main__':
-    args = [int(input())]
-    args.append(list(map(int, input().split())))
+    args = [[int(i) % 3 for i in input()]]
     main(*args)
