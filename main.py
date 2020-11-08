b0bed5a0f36d1b83070aeda72107c29a24642c1d
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-08 21:13:14 +0900

    ABC182-B

--- main.py
+++ main.py
@@ -4,12 +4,24 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-
 def main(*args):
-    A, B = args
-    print(2 * A + 100 - B)
+    N, A = args
+
+    ans = 0
+    maximum = 0
+    for i in range(2, max(A)+1):
+        cnt = 0
+        for a in A:
+            if a % i == 0:
+                cnt += 1
+        else:
+            if cnt > maximum:
+                maximum = cnt
+                ans = i
+    print(ans)
     
 
 if __name__ == '__main__':
-    args = list(map(int, input().split()))
+    args = [int(input())]
+    args.append(list(map(int, input().split())))
     main(*args)
