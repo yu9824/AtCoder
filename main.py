1a91b800a1bf88908915f65ef32db64923079169
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-15 21:10:25 +0900

    ABC183-B

--- main.py
+++ main.py
@@ -4,9 +4,15 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
+from decimal import Decimal
 
 def main(*args):
-    print(max(int(input()), 0))
+    S = tuple(map(Decimal, args[:2]))
+    G = tuple(map(Decimal, args[2:]))
+    ans = (G[0] - S[0]) * S[1] / (G[1] + S[1]) + S[0]
+
+    print('{:.7f}'.format(ans))
 
 if __name__ == '__main__':
-    main()
+    args = list(map(int, input().split()))
+    main(*args)
