438b1c19a7582a7fc185a099625174d4b818296a
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-21 21:43:47 +0900

    ARC108-B

--- main.py
+++ main.py
@@ -4,19 +4,34 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-from math import ceil, floor
-from decimal import Decimal
+from collections import deque
 
 def main(*args):
-    S, P = args
-    r = Decimal(S ** 2 - 4 * P).sqrt()
-    ans = (S + r) / Decimal(2), (S - r) / Decimal(2)
-    # '{:.20f}'.format(ans[0])
-    if floor(ans[0]) == ceil(ans[0]) and floor(ans[1]) == ceil(ans[1]):
-        print('Yes')
-    else:
-        print('No')
+    N, S = args
+
+    counta = 0  # 何個の 'fox' があったか
+    active = deque()    # activeな'f', 'o'があるかを保存しておくque
+    for n in range(N):
+        # print(active)
+        if S[n] == 'f':
+            active.append('f')
+            continue
+        elif len(active) == 0:
+            pass
+        elif S[n] == 'o':
+            if active[-1] == 'f':
+                active.append('o')
+                continue
+        elif S[n] == 'x':
+            if active[-1] == 'o':
+                active.pop()
+                active.pop()
+                counta += 1
+                continue
+        active.clear()
+    print(N - counta * 3)
+
 
 if __name__ == '__main__':
-    args = list(map(int, input().split()))
+    args = [int(input()), input()]
     main(*args)
