229eb90de98513d06a12612b180da05b9b2ed3f3
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-22 22:30:07 +0900

    ABC184-C

--- main.py
+++ main.py
@@ -5,20 +5,63 @@ import sys
 sys.setrecursionlimit(10 ** 9)
 
 
-def main(*args):
-    N, X, S = args
+# '{:.7f}'.format(i)
+
+# def main(*args):
+#     A, B, C = sorted(args)  # Cが一番大きい
+#     s = A + B + C
+#     P = (A / s, B / s, C / s)
+#     print(100 - C, '回から', 300 - s - 2, '回までに終わる．')
+
+#     ans = 0
+#     for j in range(100-C, 300 - s - 2 + 1):
+#         ans += P[2] * (100 - C)
+#         ans += P[1] * (100 - C - )
+
+    # print(((100 - A) * A + (100 - B) * B + (100 - C) * C) / s)
     
-    ans = X
-    n = 0
-    while n < N:
-        if S[n] == 'o':
-            ans += 1
+    # def cal(A, j):
+    #     return (A + j) / (s + j)
+
+    # def get_sum(A):
+    #     return sum(map(cal, [A] * (100 - A), range(0, 100-A)))
+    # a = get_sum(A)
+    # b = get_sum(B)
+    # c = get_sum(C)
+    # print('{:.7f}'.format(a + b + c))
+    
+    
+
+# if __name__ == '__main__':
+#     args = list(map(int, input().split()))
+#     main(*args)
+
+
+
+
+
+def main(*args):
+    r1, c1, r2, c2 = args
+
+    # 原点移動させて考える
+    now = [r2 - r1, c2 - c1]
+    if now == [0, 0]:
+        cnt = 0
+    elif abs(now[0]) <= 3 and abs(now[1]) <= 3 and abs(now[0]) + abs(now[1]) <= 3:
+        cnt = 1
+    else:
+        if abs(now[0] - now[1]) == 0 or abs(now[0] + now[1]) == 0:
+            cnt = 1
+        elif abs(now[0] - now[1]) <= 3 or abs(now[0] + now[1]) <= 3:
+            cnt = 2
+        elif sum(now) % 2 == 0:
+            cnt = 2
         else:
-            ans = max(0, ans - 1)
-        n += 1
-    print(ans)
+            cnt = 3
+    print(cnt)
+    
 
 if __name__ == '__main__':
-    args = list(map(int, input().split())) + [input()]
+    args = list(map(int, input().split())) + list(map(int, input().split()))
     main(*args)
     
