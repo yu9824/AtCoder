8d18cbce802824f60082a61d0d8b7d2ad253003b
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-22 21:01:25 +0900

    ABC184-A

--- main.py
+++ main.py
@@ -4,29 +4,12 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-'''
-DP
-A[n] = A[n-3] + A[n-4] + ... + A[0] (O(S**2))
-ここで，A[n-1] = A[n-4] + A[n-5] + ... + A[0]より，
-A[n] = A[n-3] + A[n-1]とも表せる．(O(S)でより高速．)
-'''
 
-mod = 10 ** 9 + 7
 def main(*args):
-    S = args[0]
-
-    A = [0 for s in range(S+1)]
-    A[0] = 1    # 何も足さない (= S自身のみの1通りを表すためのやつ．)
-    
-    s = 3
-    while s <= S:
-        # A[s] = sum(A[:(s-3)+1]) % mod # どっちでもOK．速いのは下のやつ．
-        A[s] = (A[s-3] + A[s-1]) % mod
-        s += 1
-    print(A[S])
-
+    a, b, c, d = args
+    print(a * d - b * c)
 
 if __name__ == '__main__':
-    args = [int(input())]
+    args = list(map(int, input().split())) + list(map(int, input().split()))
     main(*args)
     
