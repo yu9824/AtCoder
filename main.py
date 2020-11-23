52d8f775dac7410f9347158feca36baccd9b5fde
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-23 17:51:01 +0900

    ABC184-E

--- main.py
+++ main.py
@@ -5,63 +5,65 @@ import sys
 sys.setrecursionlimit(10 ** 9)
 
 
-# '{:.7f}'.format(i)
+# 幅優先探索
+# pythonだとTLE, pypyで通った．
+from collections import deque
 
-# def main(*args):
-#     A, B, C = sorted(args)  # Cが一番大きい
-#     s = A + B + C
-#     P = (A / s, B / s, C / s)
-#     print(100 - C, '回から', 300 - s - 2, '回までに終わる．')
-
-#     ans = 0
-#     for j in range(100-C, 300 - s - 2 + 1):
-#         ans += P[2] * (100 - C)
-#         ans += P[1] * (100 - C - )
-
-    # print(((100 - A) * A + (100 - B) * B + (100 - C) * C) / s)
-    
-    # def cal(A, j):
-    #     return (A + j) / (s + j)
-
-    # def get_sum(A):
-    #     return sum(map(cal, [A] * (100 - A), range(0, 100-A)))
-    # a = get_sum(A)
-    # b = get_sum(B)
-    # c = get_sum(C)
-    # print('{:.7f}'.format(a + b + c))
-    
-    
-
-# if __name__ == '__main__':
-#     args = list(map(int, input().split()))
-#     main(*args)
-
-
-
-
+# 定数
+code_a = ord('a')
 
 def main(*args):
-    r1, c1, r2, c2 = args
+    H, W, A, S, G, tele = args
+    # print(H, W, A, S, G, tele)
 
-    # 原点移動させて考える
-    now = [r2 - r1, c2 - c1]
-    if now == [0, 0]:
-        cnt = 0
-    elif abs(now[0]) <= 3 and abs(now[1]) <= 3 and abs(now[0]) + abs(now[1]) <= 3:
-        cnt = 1
-    else:
-        if abs(now[0] - now[1]) == 0 or abs(now[0] + now[1]) == 0:
-            cnt = 1
-        elif abs(now[0] - now[1]) <= 3 or abs(now[0] + now[1]) <= 3:
-            cnt = 2
-        elif sum(now) % 2 == 0:
-            cnt = 2
-        else:
-            cnt = 3
-    print(cnt)
+    # 移動候補
+    d = ((0, 1), (1, 0), (0, -1), (-1, 0))
+
+
+    cost = [[float('inf') for w in range(W)] for h in range(H)]
+    cost[S[0]][S[1]] = 0
+    # print(cost)
+
+    que = deque([S])
+    while que:
+        x, y = que.popleft()
+        nextcost = cost[x][y] + 1
+        for dx, dy in d:
+            x2 = x + dx
+            y2 = y + dy
+            if x2 < 0 or x2 >= H or y2 < 0 or y2 >= W or A[x2][y2] == '#':  # 範囲外 or 移動できない場所だった場合．
+                continue
+            if cost[x2][y2] > nextcost: # まだ訪れていない or これより高コストで来ることになっている場合
+                cost[x2][y2] = nextcost
+                que.append((x2, y2))    # 移動先候補として追加
+        if A[x][y].islower():   # テレポートできるなら
+            i = ord(A[x][y]) - code_a
+            for x2, y2 in tele[i]:
+                if cost[x2][y2] > nextcost: # まだ訪れていない or これより高コストで来ることになっている場合
+                    cost[x2][y2] = nextcost
+                    que.append((x2, y2))    # 移動先候補として追加
+            tele[i].clear() # 一回使ったテレポーターは戻るのに使われるだけで最短ルートには不要なのでクリア．
+    ans = cost[G[0]][G[1]]
+    if ans == float('inf'):
+        ans = -1
+    print(ans)
     
 
 if __name__ == '__main__':
-    args = list(map(int, input().split())) + list(map(int, input().split()))
+    H, W = list(map(int, input().split()))
+    args = [H, W]
+    A = []
+    tele = [[] for i in range(26)]
+    for h in range(H):
+        a = input()
+        for w in range(W):
+            if a[w] == 'S':
+                S = [h, w]
+            elif a[w] == 'G':
+                G = [h, w]
+            elif a[w].islower():
+                tele[ord(a[w])-code_a].append((h, w))
+        A.append(a)
+    args.extend([A, S, G, tele])
     main(*args)
     
