2998a08f6a1f6b18e8ad5f1a28b824b40f0bd462
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-28 21:13:57 +0900

    ARC109-A

--- main.py
+++ main.py
@@ -4,33 +4,14 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-
-# メモ再帰DP
-# きちんとした立式ができるかどうかな気がする．
-
-# 定数
-code_a = ord('a')
-
 def main(*args):
-    A, B, C = args
+    a, b, x, y = args
+    if a <= b:
+        ans = (b - a) * min(y, 2 * x) + x
+    else:   # a > b
+        ans = (a - b - 1) * min(y, 2 * x) + x
 
-    def cal(X, Y, Z, memo = {}):
-        S = X + Y + Z
-        key = (X, Y, Z)
-        if 100 in key:
-            return 0
-        elif key in memo:
-            return memo[key]
-        else:
-            X2 = X / S * (cal(X+1, Y, Z, memo) + 1) if X else 0
-            Y2 = Y / S * (cal(X, Y+1, Z, memo) + 1) if Y else 0
-            Z2 = Z / S * (cal(X, Y, Z+1, memo) + 1) if Z else 0
-            value = X2 + Y2 + Z2
-            memo[key] = value
-            return value
-
-    ans = cal(A, B, C)
-    print('{:.7f}'.format(ans))
+    print(ans)
     
 
 if __name__ == '__main__':
