788aeb7c8ccfa588ef5ef76bdb28f1c6c2607276
yu9824 <58211916+yu9824@users.noreply.github.com>
2020-11-28 21:29:25 +0900

    ARC109-B

--- main.py
+++ main.py
@@ -4,17 +4,25 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-def main(*args):
-    a, b, x, y = args
-    if a <= b:
-        ans = (b - a) * min(y, 2 * x) + x
-    else:   # a > b
-        ans = (a - b - 1) * min(y, 2 * x) + x
+from math import sqrt, ceil
 
+def main(*args):
+    n = args[0]
+
+    # 長さn + 1の丸太を買うことで何本 (i本) 節約できるかを計算．
+    s = lambda x: x * (x + 1) // 2  # 総和求める関数
+    
+    # iを全部やってるとめっちゃ時間かかるのであたりをつける．
+    i = ceil(sqrt(2 * (n + 1)))
+    while s(i) >= n + 1:
+        i -= 1
+    
+    # n+1の長さの丸太から，小さいほうからi本のマルタを作ることができる（捨ててOKなので)
+    ans = n - i + 1
     print(ans)
     
 
 if __name__ == '__main__':
-    args = list(map(int, input().split()))
+    args = [int(input())]
     main(*args)
     
