220a02d7c289773dd18608be8bb45c1914e27b26
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-01-02 21:41:34 +0900

    ABC187-D

--- main.py
+++ main.py
@@ -4,28 +4,49 @@ def I(): return int(input())
 import sys
 sys.setrecursionlimit(10 ** 9)
 
+'''
+青木派でsortして上から順番に寝返り？
+得失点差でsortして上から順番に寝返り？
+'''
 def main(*args):
-    N, S = args
+    N, AB = args
 
-    st = set()
-    for s in S:
-        if s not in st:
-            if '!' in s:
-                st.add(s[1:])
-            else:
-                st.add(s)
-        else:
-            if '!' in s:
-                print(s[1:])
-            else:
-                print(s)
+    # aoki = []
+    # takahashi = []
+    
+    # for a, b in AB:
+    #     takahashi.append(a+b)
+    #     aoki.append(a)
+
+    # 得失点差を求める
+    # tokushittenn = [t + a for t, a in zip(takahashi, aoki)]
+
+
+    # 得失点差を求める
+    tokushittenn = []
+    aoki = 0
+    for a, b in AB:
+        tokushittenn.append(a + b + a)
+        aoki += a
+    # print(aoki, tokushittenn)
+
+    # 得失点順を降順に並べ替えて，その順番に演説していく（効果的な街の順番）
+    for i, city in enumerate(argsort(tokushittenn, reverse = True)):
+        aoki -= tokushittenn[city]
+        if aoki < 0:
+            print(i+1)
             break
-    else:
-        print('satisfiable')
+        
+
+def argsort(seq, reverse = False):
+    # http://stackoverflow.com/questions/3071415/efficient-method-to-calculate-the-rank-vector-of-a-list-in-python
+    # https://stackoverflow.com/questions/3382352/equivalent-of-numpy-argsort-in-basic-python
+    return sorted(range(len(seq)), key=seq.__getitem__, reverse = reverse)
+
 
 
 if __name__ == '__main__':
     N = int(input())
     args = [N]
-    args.append({input() for n in range(N)})
+    args.append([LI() for n in range(N)])
     main(*args)
