fff4f3fcb470af04ba539d4d63f41f90e3b8e40c
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-01-16 21:43:18 +0900

    keyence2021-A

--- main.py
+++ main.py
@@ -5,17 +5,11 @@ sys.setrecursionlimit(10 ** 9)
 
 
 def main(*args):
-    N, A = args
-
-    half = 2 ** (N-1)
-
-    kessho = max(A[:half]), max(A[half:])
-    print(A.index(min(kessho)) + 1)
-
-    
+    N, K, A = args
     
 
 if __name__ == '__main__':
-    args = [int(input())]
+    N, K = LI()
+    args = [N, K]
     args.append(LI())
     main(*args)
