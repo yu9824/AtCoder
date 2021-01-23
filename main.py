dd4a4f6227d344e59cf0ec44f437fe6dda462915
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-01-23 22:15:48 +0900

    ABC189-C

--- main.py
+++ main.py
@@ -2,23 +2,52 @@ def LI(): return list(map(int, input().split()))
 
 import sys
 sys.setrecursionlimit(10 ** 9)
-from decimal import Decimal
+
+from collections import deque
+
+# http://prdc.hatenablog.com/entry/2017/09/12/124845
+# http://algorithms.blog55.fc2.com/blog-entry-132.html
 
 def main(*args):
-    N, X, VP = args
-    s = Decimal(0)
-    for i, (v, p) in enumerate(VP):
-        s += Decimal(v) * Decimal(p) / Decimal(100)
-        if s > X:
-            print(i+1)
-            break
-    else:
-        print(-1)
+    N, A = args
+    A += [0]
+
+    # ヒストグラムと考えた (左端位置, 高さ) を追加していく
+    que = deque()
+    result = 0
+    
+    for r, a in enumerate(A):
+        if que: # queに何か入っていたら
+            b = que[-1][1]
+            if a > b:
+                que.append([r, a])
+            elif a < b:
+                while a <= que[-1][1]:
+                    l, b = que.pop()
+                    result = max(result, (r - l) * b)
+                    if not que:
+                        break
+                # 最後にqueから取り出した左端位置を使って追加，
+                que.append([l, a])
+        else:   # queが空だったら
+            que.append([r, a])
+            
+    print(result)
 
     
 
 if __name__ == '__main__':
-    N, X = LI()
-    args = [N, X]
-    args.append([LI() for n in range(N)])
+    N = int(input())
+    args = [N]
+    args.append(LI())
+    
+    # N = 6
+    # A = [1, 3, 3, 5, 7, 2]
+    # args = [N, A]
+
+    # import numpy as np
+    # N = 10 ** 4
+    # A = np.random.randint(1, 10 ** 5 + 1, N).tolist()
+    # args = [N, A]
+
     main(*args)
