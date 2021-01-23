122b7fbe1663327215f12f20f9915f7c74e9866f
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-01-23 21:13:20 +0900

    ABC189-B

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ def LI(): return list(map(int, input().split()))
 
 import sys
 sys.setrecursionlimit(10 ** 9)
+from decimal import Decimal
 
 def main(*args):
-    C = set(list(input()))
-    if len(C) == 1:
-        print('Won')
+    N, X, VP = args
+    s = Decimal(0)
+    for i, (v, p) in enumerate(VP):
+        s += Decimal(v) * Decimal(p) / Decimal(100)
+        if s > X:
+            print(i+1)
+            break
     else:
-        print('Lost')
+        print(-1)
+
     
 
 if __name__ == '__main__':
-    args = []
+    N, X = LI()
+    args = [N, X]
+    args.append([LI() for n in range(N)])
     main(*args)
