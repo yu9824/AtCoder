71a91720f82699c3e61c6e5c1dedc5dd66afa8a2
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-01-23 21:03:30 +0900

    ABC189-A

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ def LI(): return list(map(int, input().split()))
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-
 def main(*args):
-    N, K, A = args
+    C = set(list(input()))
+    if len(C) == 1:
+        print('Won')
+    else:
+        print('Lost')
     
 
 if __name__ == '__main__':
-    N, K = LI()
-    args = [N, K]
-    args.append(LI())
+    args = []
     main(*args)
