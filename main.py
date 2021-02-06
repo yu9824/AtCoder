514a18a405dcd37837575d9146e633361228f3c3
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-02-06 21:34:08 +0900

    ABC191-D

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ def LI(): return list(map(int, input().split()))
 import sys
 sys.setrecursionlimit(10 ** 9)
 
+from math import ceil, floor, sqrt
+from decimal import Decimal
 
 def main(*args):
-    N, X, A = args
+    X, Y, R = args
+    x_low = ceil(X - R)
+    x_high = floor(X + R)
 
-    ans = [str(a) for a in A if a != X]
-    print(' '.join(ans))
-    
+    ans = 0
+    f = lambda x: ((R ** 2 )- ((X - x) ** 2)).sqrt()
+    for x in range(x_low, x_high+1):
+        p = f(x)
+        y_low = ceil(Y - p)
+        y_high = floor(Y + p)
+        ans += y_high - y_low + 1
+    print(ans)    
 
 if __name__ == '__main__':
-    args = LI()
-    args.append(LI())
+    args = list(map(Decimal, input().split()))
     main(*args)
