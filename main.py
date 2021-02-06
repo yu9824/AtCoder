5f816617b9a79f07d08a33b600e8130fe22bbd8f
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-02-06 21:02:21 +0900

    ABC191-A

--- main.py
+++ main.py
@@ -3,20 +3,14 @@ def LI(): return list(map(int, input().split()))
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-from math import ceil
 
 def main(*args):
-    N, S, D, XY = args
-    for x, y in XY:
-        if x < S and y > D:
-            print('Yes')
-            break
-    else:
+    V, T, S, D = args
+    if V * T <= D <= V * S:
         print('No')
+    else:
+        print('Yes')
 
 if __name__ == '__main__':
-    N, S, D = LI()
-    args = [N, S, D]
-    args.append([LI() for n in range(N)])
-
+    args = LI()
     main(*args)
