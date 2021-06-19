5ecb89c853d6cc8b1e5b68e56c444cf891234db8
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-06-19 15:56:58 +0900

    typical90_a error

--- main.py
+++ main.py
@@ -3,12 +3,53 @@ def LI(): return list(map(int, input().split()))
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-# def main(*args):
-    # pass    
+'''
+# 思ったこと
+10**5なので，combinationをつかって総当たりは無理そう．
+長さが平均になるように切れるようにするのが最適解な気がする． (L/(K+1))
+後ろからと前からL/(K+1)より小さく切れるところで切っていった結果を比較すればいける？
+
+いや．普通に差だけ比較すれば良いのでは？
+
+わからないので回答を見る．https://twitter.com/e869120/status/1377027868518064129
+# 答えで二分探索 がキーワード
+「後ろからと前からL/(K+1)より小さく切れるところで切っていった結果を比較すればいける？」の考え方が惜しかった．この閾値を可変にして二分探索すればできた．
+算数みたいに答えを直接答えを出すことにこだわってしまったが，そうとは限らないということか．
+'''
+
+# https://atcoder.jp/contests/typical90/tasks/typical90_a
+
+from math import ceil
+
+def main(*args):
+    N, L, K, A = args
+
+    low = 0
+    high = ceil(L / (K+1))
+
+    while high - low > 1:
+        mid = (low + high) // 2
+        cnt = 0 # 何個切れ目を入れたかのカウント
+        x = 0
+        for i in range(N):
+            if A[i] - x >= mid:
+                cnt += 1
+                x = A[i]
+        if cnt >= K and L - x >= mid:   # 最後の最後も条件を満たしていれば 
+            low = mid
+        else:   # cnt < K
+            high = mid
+        print(high, low)
+    print(mid)
+
+            
+                
+            
 
 
-# if __name__ == '__main__':
-#     main(args)
+if __name__ == '__main__':
+    N, L = LI()
+    K = int(input())
+    A = LI()
+    main(N, L, K, A)
 
-s = input()
-print(s[0] + str(len(s)-2) + s[-1])
