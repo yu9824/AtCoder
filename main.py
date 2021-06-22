9b6403e37322758bd5b4eeba72501c84a7e96919
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-06-22 09:45:21 +0900

    typical90_d failed.

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ O(HW)でもギリいける?
 
 横着してnumpy + numbaでやろうとするとTLE(cache=Trueで14AC, 2TLE)
 横着しなくてもTLE(なんなら10AC, 6TLEなので結果が悪い)
+PyPyで8AC, 8TLEで解決できない．
+
+解答をみる: https://twitter.com/e869120/status/1378115289649348611
+やり方は完全にあっている．ただ，できない．
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_d
@@ -17,15 +21,10 @@ O(HW)でもギリいける?
 def main(*args):
     H, W, A = args
 
-    sum_W = []
-    sum_H = []
-    for w in range(W):
-        sum_W.append(sum(list(zip(*A))[w])) # sum(list(*A))でtranspose()している．
-    for h in range(H):
-        sum_H.append(sum(A[h]))
+    sum_W = [(sum(list(zip(*A))[w])) for w in range(W)] # sum(list(*A))でtranspose()している．
+    sum_H = [sum(A[h]) for h in range(H)]
     
-    for h in range(H):
-        print(' '.join([str(sum_W[w] + sum_H[h] - A[h][w]) for w in range(W)]))
+    [print(' '.join([str(sum_W[w] + sum_H[h] - A[h][w]) for w in range(W)])) for h in range(H)]
     
 
 if __name__ == '__main__':
