0802dad3d9a7b34d2201ad59b014f79c8462b640
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-06-22 09:26:58 +0900

    typical_d failed. I tried to use numpy + numba, but it turned out to be a TLE.

--- main.py
+++ main.py
@@ -5,20 +5,46 @@ sys.setrecursionlimit(10 ** 9)
 
 '''
 # 思ったこと
-
+事前に縦と横の合計しらべればいいのでは？そのあと取り出すイメージ．
+O(HW)でもギリいける?
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_d
 
-
+import numpy as np
+from numba import jit
 
 def main(*args):
     H, W, A = args
-    print(H, W, A)
+    A = np.array(A, dtype=int)
+
+    answers = get_answers(A)
+    for h in range(H):
+        print(' '.join(answers[h].astype(str)))
+
+    # A = sumH + sumW - A
+
+    # sum_W = []
+    # sum_H = []
+    # for w in range(W):
+    #     print(A[:, w])
+    #     sum_W.append(sum(A[:][w]))
+    # for h in range(H):
+    #     sum_H.append(sum(A[h]))
+    # print(sum_W, sum_H)
+    
+    # for h in range(H):
+    #     print(' '.join([str(sum_W[w] + sum_H[h] - A[h][w]) for w in range(W)]))
+
+@jit('i8[:,:](i8[:,:])')
+def get_answers(A):
+    sumH = A.sum(axis=1).reshape(-1, 1)
+    sumW = A.sum(axis=0)
+    return sumH + sumW - A
     
 
 if __name__ == '__main__':
     H, W = LI()
-    A = [LI() for w in range(W)]
+    A = [LI() for h in range(H)]
     main(H, W, A)
 
