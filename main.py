d800eab7fc0db67d4a50070ec21b7bd92c6b4f19
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-06-22 09:51:43 +0900

    typical90_d

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ PyPyで8AC, 8TLEで解決できない．
 
 解答をみる: https://twitter.com/e869120/status/1378115289649348611
 やり方は完全にあっている．ただ，できない．
+
+*** sum(list(*A))がめちゃくちゃ重かったっぽい．***
+参考: https://gitlab.com/w0mbat/kyopro_educational_90/-/blob/main/004.py
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_d
@@ -21,7 +24,7 @@ PyPyで8AC, 8TLEで解決できない．
 def main(*args):
     H, W, A = args
 
-    sum_W = [(sum(list(zip(*A))[w])) for w in range(W)] # sum(list(*A))でtranspose()している．
+    sum_W = [sum(A[h][w] for h in range(H)) for w in range(W)] # sum(list(*A))でtranspose()している．←これがTLEの元凶だったっぽい．これを変えて，PyPyにしたところめっちゃ早くなった．listからジェネレータによる計算に変わったことが要因？
     sum_H = [sum(A[h]) for h in range(H)]
     
     [print(*[str(sum_W[w] + sum_H[h] - A[h][w]) for w in range(W)]) for h in range(H)]
