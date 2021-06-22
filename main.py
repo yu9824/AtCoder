1207678dcb91f48264372a93059a667064ba2472
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-06-22 09:36:30 +0900

    typical90_d failed.

--- main.py
+++ main.py
@@ -7,40 +7,25 @@ sys.setrecursionlimit(10 ** 9)
 # 思ったこと
 事前に縦と横の合計しらべればいいのでは？そのあと取り出すイメージ．
 O(HW)でもギリいける?
+
+横着してnumpy + numbaでやろうとするとTLE(cache=Trueで14AC, 2TLE)
+横着しなくてもTLE(なんなら10AC, 6TLEなので結果が悪い)
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_d
 
-import numpy as np
-from numba import jit
-
 def main(*args):
     H, W, A = args
-    A = np.array(A, dtype=int)
 
-    answers = get_answers(A)
+    sum_W = []
+    sum_H = []
+    for w in range(W):
+        sum_W.append(sum(list(zip(*A))[w])) # sum(list(*A))でtranspose()している．
     for h in range(H):
-        print(' '.join(answers[h].astype(str)))
-
-    # A = sumH + sumW - A
-
-    # sum_W = []
-    # sum_H = []
-    # for w in range(W):
-    #     print(A[:, w])
-    #     sum_W.append(sum(A[:][w]))
-    # for h in range(H):
-    #     sum_H.append(sum(A[h]))
-    # print(sum_W, sum_H)
+        sum_H.append(sum(A[h]))
     
-    # for h in range(H):
-    #     print(' '.join([str(sum_W[w] + sum_H[h] - A[h][w]) for w in range(W)]))
-
-@jit('i8[:,:](i8[:,:])')
-def get_answers(A):
-    sumH = A.sum(axis=1).reshape(-1, 1)
-    sumW = A.sum(axis=0)
-    return sumH + sumW - A
+    for h in range(H):
+        print(' '.join([str(sum_W[w] + sum_H[h] - A[h][w]) for w in range(W)]))
     
 
 if __name__ == '__main__':
