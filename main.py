7dfee0be82689c6a57d4ef263197c41676080949
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-06-27 13:26:04 +0900

    typical90_h

--- main.py
+++ main.py
@@ -22,31 +22,40 @@ sys.setrecursionlimit(10 ** 9)
 考え方が難しい
 - 自分を採用する場合: 左上と横を受け継ぐ
 - 自分を採用しない場合: 横だけ受け継ぐ
+
+全然実装できなかったが，勘違いしている点があった．
+- dp[pos][atcoderの何文字目まで] = 通り数
+    - このとき，0 ~ len(ATCODER)で，0は0-indexの0ではなく，1-indexの0である．つまり，まだ何も選んでいない状態は何通りか, i.e. 1通りと考えることができる．
+    - そのため，dp.shape(N+1, len(ATCODER)+1)
+- 全部 += だと思ってたら必ずしもそうしなくてもよかったっぽい．
+
+最終的な参考回答: https://qiita.com/toast-uz/items/bf6f142bace86c525532#44-%E8%80%B3dp
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_h
 
-
 MOD = 10**9+7
+
 def main(*args):
     N, S = args
 
     ATCODER = "atcoder"
-
-    dp = [[0 for __ in range(N)] for _ in ATCODER]
+    dp = [[0 for __ in ' ' + ATCODER] for _ in range(N+1)]
     
-    for n in range(N):
-        for idx in range(len(ATCODER)):
-            if S[n] == ATCODER[idx]:    # 採用可能な場合
-                if idx == 0:    # 'a'だった場合
-                    dp[idx][n] = 1
-                elif n > 0: # idx > 0のとき，n > 0でしか厳密には採用できないので．
-                    dp[idx][n] += (dp[idx-1][n-1] + dp[idx][n-1]) % MOD
-            elif n > 0:   # 採用できない場合かつn > 0とき
-                dp[idx][n] += dp[idx][n-1]
-
-    print(dp)
-        
+    for i in range(N+1):
+        for j in range(len(ATCODER)+1):
+            if j == 0:
+                dp[i][j] = 1
+            elif i == 0:
+                pass    # 初期値が0なので．
+            elif S[i-1] == ATCODER[j-1]:
+                dp[i][j] = (dp[i-1][j] + dp[i-1][j-1]) % MOD
+            else:
+                dp[i][j] = dp[i-1][j]
+    
+    # 回答に載ってる図のような形で出力
+    # import pandas as pd; print(pd.DataFrame(dp, columns = list(' ' + ATCODER), index = list(S+' ')).transpose())
+    print(dp[-1][-1])
 
 if __name__ == '__main__':
     N = int(input())
