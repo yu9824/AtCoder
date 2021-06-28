a4793c76f092dddf401b378c6bd084ee7864bb11
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-06-29 08:25:40 +0900

    Add 016.py

--- typical90/016.py
+++ typical90/016.py
@@ -5,7 +5,16 @@ sys.setrecursionlimit(10 ** 9)
 
 '''
 # 思ったこと
+O(N^2)でいける？
 
+ローカルで実行している感じできる気がしなくて解説を見る．
+解説: https://twitter.com/e869120/status/1383189464650981378
+
+考え方はほとんど合っていた．
+PyPy: 829 ms
+Python: TLE
+
+計算量計算でいけると思ったらPyPyで投げてみるのが吉なよう．
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_p
@@ -13,7 +22,18 @@ sys.setrecursionlimit(10 ** 9)
 
 def main(*args):
     N, A, B, C = args
-    print(N, A, B, C)
+    A, B, C = sorted([A, B, C])
+    
+    LIM = 9999
+    ans = LIM
+    for j in range(LIM):
+        for k in range(LIM-j+1):
+            rest = N - (j*A + k*B)
+            if rest % C == 0 and rest >= 0:
+                l = rest // C
+                ans = min(ans, j+k+l)
+    print(ans)
+
     
 
 if __name__ == '__main__':
