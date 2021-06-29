4db40f3914ecd0aec795d6d4b9690bf173411846
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-06-29 09:04:18 +0900

    Add HHKB2020-A

---
+++ ABC/HHKB2020/A.py
@@ -0,0 +1,15 @@
+# list(map(int, input().split()))
+# int(input())
+
+from pdb import set_trace
+
+def main():
+    S = input()
+    T = input()
+    if S == 'Y':
+        print(T.upper())
+    else:
+        print(T)
+
+if __name__ == '__main__':
+    main()
