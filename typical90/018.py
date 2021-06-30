14d2e80e1f1090517693d5419052de972d1fdec2
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-06-30 10:18:52 +0900

    Add 018.py

--- typical90/018.py
+++ typical90/018.py
@@ -5,15 +5,31 @@ sys.setrecursionlimit(10 ** 9)
 
 '''
 # 思ったこと
+ガチで円運動では？
+y = -(L/2) * sin(omega * t)
+z = (L/2) * (1 - cos(omega * t))
 
+なんか通った．
+誤差に言及されていたのでDecimal必要かと思ったけどそんなことはなかった．
+
+Python: 30 ms
+PyPy: 85 ms
+
+解説: https://twitter.com/e869120/status/1384276005330690049/
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_r
 
-
+from math import sin, cos, pi, asin, sqrt
 def main(*args):
     T, L, X, Y, Q, E = args
-    print(T, L, X, Y, Q, E)
+    omega = 2 * pi / T
+    get_y = lambda t:-(L/2) * sin(omega * t)
+    get_z = lambda t:(L/2) * (1 - cos(omega * t))
+    for e in E:
+        y1 = get_y(e)
+        z1 = get_z(e)
+        print(asin(z1 / sqrt(X ** 2 + (Y - y1) ** 2 + z1 ** 2))/pi*180)
 
 if __name__ == '__main__':
     T = int(input())
