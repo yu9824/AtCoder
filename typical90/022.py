32da39dfbab9990c3a9f1301cd214dfe57c6c46b
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-01 10:08:18 +0900

    Add 022.py

--- typical90/022.py
+++ typical90/022.py
@@ -5,13 +5,28 @@ sys.setrecursionlimit(10 ** 9)
 
 '''
 思ったこと
+各辺がすべて最大公約数になるように切ればいいのでは？
 
+Python: 34 ms
+PyPy: 85 ms
+
+解説: https://twitter.com/e869120/status/1385725481920520193
+ユークリッドの互除法を学ぶためのものだった．最大公約数の考え方が合っていたのは嬉しい．
+正直，何回やっても忘れちゃうし，Pythonには標準ライブラリで搭載されているので，忘れてしまいそう．．．
+
+計算量がO(log(A+B+C))ってのは覚えておいたほうがいいかも．
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_v
 
+from functools import reduce
+from math import gcd
 def main(*args):
-    A, B, C = args
+    GCD = reduce(gcd, args)
+    ans = 0
+    for x in args:
+        ans += x // GCD - 1 # たとえば2個に切るとき，1回切れば2個になる，という考え方
+    print(ans)
 
 if __name__ == '__main__':
     main(*LI())
