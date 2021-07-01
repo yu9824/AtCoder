3efb549bd059b92d87397cc72573e838cb813711
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-01 09:53:20 +0900

    preparation for 022.py

---
+++ typical90/022.py
@@ -0,0 +1,17 @@
+def LI(): return list(map(int, input().split()))
+
+import sys
+sys.setrecursionlimit(10 ** 9)
+
+'''
+思ったこと
+
+'''
+
+# https://atcoder.jp/contests/typical90/tasks/typical90_v
+
+def main(*args):
+    A, B, C = args
+
+if __name__ == '__main__':
+    main(*LI())
