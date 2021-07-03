630df119c41b2d37521fe62df25ca6417948dd39
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-03 09:31:35 +0900

    024.py failed.

--- typical90/024.py
+++ typical90/024.py
@@ -5,14 +5,23 @@ sys.setrecursionlimit(10 ** 9)
 
 '''
 思ったこと
+K10**9だけど全探索で該当するものを選ぶってのはできるかな．
 
+いや，普通に絶対値の引き算するだけで良いのでは？
+それでdiff < Kかつ (K - diff) % 2 == 0 (打ち消し合う動作で無駄に回数を消費できる)だったらいける？
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_x
 
 def main(*args):
     N, K, A, B = args
-    print(N, K, A, B)
+    diff = 0
+    for a, b in zip(A, B):
+        diff += abs(a-b) 
+    if K > diff and (K - diff) % 2 == 0:
+        print('Yes')
+    else:
+        print('No')
 
 if __name__ == '__main__':
     main(*LI(), LI(), LI())
