e10b0ce27a2c0e1502873a382ad11cb5bfb10327
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-04 21:46:06 +0900

    Add ABC208-B

--- ABC/208/B.py
+++ ABC/208/B.py
@@ -10,9 +10,31 @@ sys.setrecursionlimit(10 ** 9)
 
 # https://atcoder.jp/contests/abc208/tasks/abc208_b
 
+from math import factorial
 def main(*args):
     P, = args
-    print(P)
+    kouka = []
+    for n in range(10):
+        kaijou = factorial(n+1)
+        if kaijou > P:
+            break
+        kouka.append(kaijou)
+    p = P
+    ans = 0
+    for kaijou in kouka[::-1]:
+        shou = p // kaijou
+        amari = p % kaijou
+        if shou <= 100:
+            ans += shou
+            p = amari
+        else:
+            shou = 100
+            ans += shou
+            p -= kaijou * shou
+        if p == 0:
+            break
+    print(ans)
+        
 
 if __name__ == '__main__':
     main(int(input()))
--- ABC/208/D.py
+++ ABC/208/D.py
@@ -5,11 +5,21 @@ sys.setrecursionlimit(10 ** 9)
 
 '''
 思ったこと
-
+有向グラフってやつでしょうか？
+隣接行列？
+N ≤ 400だから普通に全探索やればいける？
 '''
 
 def main(*args):
-    a, b, c = args
+    N, M, ABC = args
+    graph = [[-1 for n in range(N)] for nn in range(N)]
+
+    # graph[a][b]: a to b
+    for a, b, c in ABC:
+        graph[a][b] = c
+    print(graph)
+
 
 if __name__ == '__main__':
-    main(*LI())
+    N, M = LI()
+    main(N, M, [list(map(lambda x:int(x)-1, input().split())) for m in range(M)])
