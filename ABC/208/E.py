a7f77fb29e2a23ac461d36b674b124705dcfcfff
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-04 20:56:42 +0900

    preparation for ABC208

---
+++ ABC/208/E.py
@@ -0,0 +1,15 @@
+def LI(): return list(map(int, input().split()))
+
+import sys
+sys.setrecursionlimit(10 ** 9)
+
+'''
+思ったこと
+
+'''
+
+def main(*args):
+    a, b, c = args
+
+if __name__ == '__main__':
+    main(*LI())
