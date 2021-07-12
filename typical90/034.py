f81bc2e0dc9f7482abfe7a1c06b0530711142faa
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-12 12:02:03 +0900

    20210712_temp

--- typical90/034.py
+++ typical90/034.py
@@ -15,6 +15,10 @@ REやTLEにはまったので解説を見た．
 
 考え方は合っていた．ただ自分のやろうとしている実装が複雑すぎるのでおかしいなとは思った．
 尺取り法というらしい．
+
+絶対増えること（単調増加）を生かして問題を解くらしい．
+
+参考コード: https://github.com/hibit-at/typical90/blob/main/34.py
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_ah
@@ -24,40 +28,15 @@ from collections import deque
 def main(*args):
     N, K, A = args
     
-    loc = {a:[] for a in set(A)}
-    l = 0   # その数字が続く一番左端
-    a = A[l]
-    a_previous = a
+    a = A[0]
     que = deque([a])
     ans = 0
+    d = {}  # なぜこの辞書が必要なのか？
 
     n = 1
     while n < N:
         a = A[n]
-        if a_previous != a:
-            r = n
-            loc[a_previous].append((l, r))
-            l = n
-            que.append(a)
-            a_previous = a
-        if len(set(que)) > K:
-            al = que.popleft()  # 先頭が候補から消える
-            l_first = loc[al][0][0]   # 一番最初にその数字が出てきた時の左端
-            r_last = loc[al][-1][-1]  # 一番最後のその数字が出てきた右端 → 次はここからスタート
-            loc[al] = []    # その数字のlocを初期化
-
-            while al in que:
-                loc[que.popleft()] = []    # その数字のlocを初期化
-            ans = max(ans, r - l_first)
-            n = r_last
-        n += 1
-    else:   # すべてのループが回った後，
-        # 最後連続していた場合，それをloc辞書に追加できていないので，それを追加してあげる．（K==1の場合にバグるのを防ぐため）
-        loc[a].append((l, n))
-        l_first = loc[que.popleft()][0][0]
-        ans = max(ans, n - l_first)
-
-    print(ans)
+        
 
 
         
