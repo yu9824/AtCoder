7218bd251cd6bfca6815f05f29a9697355244619
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-13 15:13:03 +0900

    034.py TLE

--- typical90/034.py
+++ typical90/034.py
@@ -19,28 +19,40 @@ REやTLEにはまったので解説を見た．
 絶対増えること（単調増加）を生かして問題を解くらしい．
 
 参考コード: https://github.com/hibit-at/typical90/blob/main/34.py
+
+$O(n^2)$ → $O(n \mathrm{log}\ n)$
+- [034.py](./034.py)
+
+- キーワード
+  - 広義単調増加関数．
+
+参考: [しゃくとり法 (尺取り法) の解説と、それを用いる問題のまとめ - Qiita](https://qiita.com/drken/items/ecd1a472d3a0e7db8dce)
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_ah
 
-from copy import copy
 from collections import deque
 def main(*args):
     N, K, A = args
     
-    a = A[0]
-    que = deque([a])
+    lx = 0
+    la = A[lx]
+    que = deque([la])
     ans = 0
-    d = {}  # なぜこの辞書が必要なのか？
 
-    n = 1
-    while n < N:
-        a = A[n]
-        
-
-
-        
-
+    rx = 1
+    while rx < N:
+        ra = A[rx]
+        que.append(ra)
+        cnt = 0
+        while len(set(que)) > K:
+            la = que.popleft()
+            cnt += 1
+        else:
+            lx += cnt
+            ans = max(ans, rx-lx+1)
+        rx += 1
+    print(ans)
 
 if __name__ == '__main__':
     N, K = LI()
