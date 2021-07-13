d85ebe209550c5dee9c14c819e9b64d8d511f0b6
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-13 15:32:29 +0900

    Add 038.py

--- typical90/038.py
+++ typical90/038.py
@@ -6,13 +6,26 @@ sys.setrecursionlimit(10 ** 9)
 '''
 思ったこと
 
+を，超える場合は，なので<=だった．
+Python: 33 ms
+
+Pythonだと簡単だけどほかだと難しいのかもしれない．
+
+問題点はgcdの実装ではなく，オーバーフローだった．
+Pythonだとあまり考えなくてもいいが，2**63-1を途中で超えてしまうとダメ，って話らしい．
 '''
 
 # https://atcoder.jp/contests/typical90/tasks/typical90_al
 
+
+from math import gcd
+def lcm(a, b): return a * b // gcd(a, b)
+
 def main(*args):
-    A, B = args
+    ans = lcm(*args)
+    ans = ans if ans <= 10 ** 18 else 'Large'
+    print(ans)
+
 
 if __name__ == '__main__':
-    N = int(input())
     main(*LI())
