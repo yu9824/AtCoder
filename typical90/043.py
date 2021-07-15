8d3281e648d0aa36aba3028199350b592144221c
yu9824 <58211916+yu9824@users.noreply.github.com>
2021-07-15 10:46:02 +0900

    failed.

--- typical90/043.py
+++ typical90/043.py
@@ -28,31 +28,40 @@ def main(*args):
 
     OK = '.'
     NG = '#'
-    lst_direction = ((1, 0), (0, 1), (-1, 0), (0, -1))
 
-    def get_candidate(x, y):
+    def get_candidate(x, y, direction_pre = -1):
+        lst_direction = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        pairs = ({1, 3}, {0, 2})    # 互いに打ち消し合うので絶対続かない．
+        
+        exclude = set()
+        for pair in pairs:
+            if direction_pre in pair:
+                exclude = {direction_pre} & pair
+                break
+        lst = []
         for direction, (k, l) in enumerate(lst_direction):
-            y_cand = y+k
-            x_cand = x+l
-            if 0 <= y_cand < H and 0 <= x_cand < W:
-                yield ((x_cand, y_cand), direction)
+            x_cand = x + k
+            y_cand = y + l
+            if 0 <= x_cand < H and 0 <= y_cand < W and direction not in exclude:
+                lst.append(((x_cand, y_cand), direction))
+        return lst
 
     dp[rs][cs] = 0
     # [(i, j), direction_pre]
     # direction_pre: lst_directionに示されたもののうちどの方角か．どれでもないときは-1．(i-1,j-1)から(i,j)にいくとき．
-    que = deque([[(cs, rs), -1]])
+    que = deque([[(rs, cs), -1]])
     while que:
         # print(que)
         (x, y), direction_pre = que.popleft()
         for (x_cand, y_cand), direction in get_candidate(x, y):
-            if S[y_cand][x_cand] == OK: # 道ならば
+            if S[x_cand][y_cand] == OK: # 道ならば
                 if direction_pre == -1 or direction_pre == direction:   # 直進してきた場合
-                    cost = dp[y][x]
+                    cost = dp[x][y]
                 else:   # 曲がる必要がある場合
-                    cost = dp[y][x] + 1
-                cost_cand = dp[y_cand][x_cand]
+                    cost = dp[x][y] + 1
+                cost_cand = dp[x_cand][y_cand]
                 if cost_cand >= cost:   # 更新できるならば
-                    dp[y_cand][x_cand] = cost
+                    dp[x_cand][y_cand] = cost
                     que.append([(x_cand, y_cand), direction])
     print(dp[rt][ct])
 
